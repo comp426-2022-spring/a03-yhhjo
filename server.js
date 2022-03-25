bad89d4fa3f75e1d123b4bcd2742295da7a9159f
yhhjo <ajb28@live.unc.edu>
2022-03-25 12:03:01 -0400

    all added and ready for autograder

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// Import coin module
+import { coinFlip, coinFlips, flipACoin } from "./modules/coin.mjs";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+
 // Require Express and minimalist
 const express = require('express')
 const app = express()
@@ -8,7 +14,44 @@ const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
 });
 
-// Define check endpoint /app/
+// Check endpoint /app/flip/call/heads/
+app.get('/app/flip/call/heads/', (req, res)=> {
+    // Respond with status 200
+    res.statusCode = 200;
+    res.statusMessage = "OK"
+    // Write JSON object
+    res.json(flipACoin("heads"))
+});
+
+// Check endpoint /app/flip/call/tails/
+app.get('/app/flip/call/tails/', (req, res)=> {
+    // Respond with status 200
+    res.statusCode = 200;
+    res.statusMessage = "OK"
+    // Write JSON object
+    res.json(flipACoin("tails"))
+});
+
+// Check endpoint /app/flips/param:[number]
+app.get('/app/flips/:number', (req, res)=> {
+    const flips = coinFlips(req.params.number)
+    // Respond with status 200
+    res.statusCode = 200;
+    res.statusMessage = "OK"
+    // Write JSON object
+    res.json(flips)
+});
+
+// Check endpoint /app/flip/
+app.get('/app/flip/', (req, res)=> {
+    // Respond with status 200
+    res.statusCode = 200;
+    res.statusMessage = "OK"
+    // Write JSON object
+    res.json(coinFlip())
+});
+
+// Check endpoint /app/
 app.get('/app/', (req, res) => {
     // Respond with status 200
     res.statusCode = 200;
@@ -16,16 +59,6 @@ app.get('/app/', (req, res) => {
     res.statusMessage = 'OK';
     res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
     res.end(res.statusCode + ' ' + res.statusMessage)
-});
-
-app.get('/app/flip', (req, res) => {
-    // Respond with status 200
-    res.statusCode = 200;
-    // Respond with status message "OK"
-    res.statusMessage = 'OK';
-    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
-    res.end(res.statusCode + ' ' + res.statusMessage)
-    // Send JSON object containing the flip
 });
 
 // Default response for any request
